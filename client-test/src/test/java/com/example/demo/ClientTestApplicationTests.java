58026b633e9b9396431e76ed9d83aaabddb7462b
Xavier <11949957@qq.com>
2020-11-23 17:31:39 +0800

    eureka client test success

---
+++ client-test/src/test/java/com/example/demo/ClientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.nuaa.microservice.clienttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
