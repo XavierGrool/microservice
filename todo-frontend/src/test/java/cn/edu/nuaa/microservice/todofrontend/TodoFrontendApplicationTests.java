06428298c75c7cce10cbdaae3404dd74a00b0971
Xavier <11949957@qq.com>
2020-11-23 23:23:29 +0800

    thymeleaf first run

---
+++ todo-frontend/src/test/java/cn/edu/nuaa/microservice/todofrontend/TodoFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.nuaa.microservice.todofrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
