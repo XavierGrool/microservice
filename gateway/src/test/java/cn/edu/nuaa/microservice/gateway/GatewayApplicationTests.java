0e6955eaad327ff04fb6cc68790d7d95ce2360ac
Xavier <11949957@qq.com>
2020-11-24 21:44:34 +0800

    gateway set up

---
+++ gateway/src/test/java/cn/edu/nuaa/microservice/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.nuaa.microservice.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
